519c91f0b9db29249d6fa0cb6532bc2d2f2d5b44
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-22 15:11:10 -0300

    feat: add Logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/rodrigofmeneses/cosmic-snap/router"
+import (
+	"github.com/rodrigofmeneses/cosmic-snap/config"
+	"github.com/rodrigofmeneses/cosmic-snap/router"
+)
+
+var logger *config.Logger
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
