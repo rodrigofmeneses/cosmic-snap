d87df3489124a2501be94c738febd5d3e238acc7
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-23 15:06:23 -0300

    feat: add list cards handler logic

--- handler/listCards.go
+++ handler/listCards.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigofmeneses/cosmic-snap/schemas"
 )
 
 func ShowCardsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Cards",
-	})
+	cards := []schemas.Card{}
+	// Find Cards
+	if err := db.Find(&cards).Error; err != nil {
+		logger.Errorf("error list cards: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error list cards in database")
+		return
+	}
+	sendSuccess(ctx, http.StatusOK, "list-cards", cards)
 }
