b606cef928ac9c4b6f37e162fe981abca48832d7
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-23 14:37:55 -0300

    feat: add create card handler logic

--- handler/createCard.go
+++ handler/createCard.go
@@ -4,10 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigofmeneses/cosmic-snap/schemas"
 )
 
 func CreateCardHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Card",
-	})
+	request := CreateCardRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	card := schemas.Card{
+		Name:        request.Name,
+		Cost:        *request.Cost,
+		Power:       *request.Cost,
+		Description: request.Description,
+		Source:      request.Source,
+		Image:       request.Image,
+	}
+
+	if err := db.Create(&card).Error; err != nil {
+		logger.Errorf("error creating card: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating card in database")
+		return
+	}
+
+	sendSuccess(ctx, http.StatusCreated, "create-card", card)
 }
--- schemas/card.go
+++ schemas/card.go
@@ -1,6 +1,10 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Card struct {
 	gorm.Model
@@ -10,5 +14,19 @@ type Card struct {
 	Description string
 	Source      string
 	Image       string
-	Tags        []string
+	// Tags        []string
+}
+
+type CardResponse struct {
+	ID          uint      `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	DeletedAt   time.Time `json:"deletedAt,omitempty"`
+	Name        string    `json:"name"`
+	Cost        int64     `json:"cost"`
+	Power       int64     `json:"power"`
+	Description string    `json:"description"`
+	Source      string    `json:"source"`
+	Image       string    `json:"image"`
+	// Tags        []string
 }
