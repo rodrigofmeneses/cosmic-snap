a0417787f1b898b944a8e9dbda356c2516bfa705
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-23 15:13:09 -0300

    feat: add show card handler logic

--- handler/showCard.go
+++ handler/showCard.go
@@ -1,13 +1,25 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigofmeneses/cosmic-snap/schemas"
 )
 
 func ShowCardHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Card",
-	})
+	id := ctx.Query("id")
+	// If not send id
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	card := schemas.Card{}
+	// Find Card
+	if err := db.First(&card, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("card with id: %s not found", id))
+		return
+	}
+	sendSuccess(ctx, http.StatusOK, "show-card", &card)
 }
