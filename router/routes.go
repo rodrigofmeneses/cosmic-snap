5b29a36c5703810b8e55e3f3bb8fa39772786c42
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-24 09:30:38 -0300

    feat: add swagger

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,19 @@
 package router
 
 import (
-	"github.com/rodrigofmeneses/cosmic-snap/handler"
-
 	"github.com/gin-gonic/gin"
+	"github.com/rodrigofmeneses/cosmic-snap/docs"
+	"github.com/rodrigofmeneses/cosmic-snap/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/card", handler.ShowCardHandler)
 		v1.POST("/card", handler.CreateCardHandler)
@@ -17,4 +21,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/card", handler.UpdateCardHandler)
 		v1.GET("/cards", handler.ShowCardsHandler)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
